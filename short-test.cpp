786eafe310b9227748500ff49f9b25bf8895db1e
Terry Lyons <tlyons@lyonstech.net>
2018-12-02 10:30:32 +0000

    Add breaking test on arrays to short-test.cpp

--- short-test.cpp
+++ short-test.cpp
@@ -7,16 +7,17 @@
 using p_var_ns::p_var;
 
 // define a type for data points, in this case R^2
-typedef std::array<double, 2> vecR2;
-
+//typedef std::array<double, 2> vecR2;
+typedef double vecR2[2];
 // define a distance function, here L^1
-double dist(vecR2 a, vecR2 b) {
+double dist(const vecR2 & a, const vecR2 & b) {
 	return std::abs(b[0]-a[0]) + std::abs(b[1]-a[1]);
 }
 
 int main () {
 	// create a path
-	std::vector<vecR2> path({{0,0}, {1,1}});
+	//std::vector<vecR2> path {{0,0}, {1,1}};
+	vecR2 path[2]{ {0,0}, {1,1} };
 
 	std::cout << "3-variation wrt L^1 distance: " << p_var(path, 3, dist) << std::endl;
 	std::cout << "3-variation wrt Euclidean distance: " << p_var(path, 3) << std::endl;
