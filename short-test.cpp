ea0086c0ffd938ec5ca43016d5f0fd9e52dd6c75
Alexey Korepanov <kaikaikai@yandex.ru>
2022-05-06 16:30:59 +0100

    replace c-style arrays with array+vector in the short example (otherwise it does not compile with cryptic error messages)

--- short-test.cpp
+++ short-test.cpp
@@ -7,8 +7,7 @@
 using p_var_ns::p_var;
 
 // define a type for data points, in this case R^2
-//typedef std::array<double, 2> vecR2;
-typedef double vecR2[2];
+typedef std::array<double, 2> vecR2;
 // define a distance function, here L^1
 double dist(const vecR2 & a, const vecR2 & b) {
 	return std::abs(b[0]-a[0]) + std::abs(b[1]-a[1]);
@@ -16,8 +15,7 @@ double dist(const vecR2 & a, const vecR2 & b) {
 
 int main () {
 	// create a path
-	//std::vector<vecR2> path {{0,0}, {1,1}};
-	vecR2 path[2]{ {0,0}, {1,1} };
+	std::vector<vecR2> path {{0,0}, {1,1}};
 
 	std::cout << "3-variation wrt L^1 distance: " << p_var(path, 3, dist).value << std::endl;
 	std::cout << "3-variation wrt Euclidean distance: " << p_var(path, 3).value << std::endl;
